899bd0b96022655773134f741d5e010b2188bc83
zsorosz <oroszi.zsofi@gmail.com>
2023-04-26 10:25:03 +0200

    Add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,43 @@
+import { useState } from "react";
 
 function Navbar(): JSX.Element {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setIsMenuOpen(!isMenuOpen);
+  };
+  console.log(isMenuOpen);
+
   return (
-    <div>Navbar</div>
-  )
+    <nav className="navbar">
+      <h1>BuckleUp</h1>
+      <div className="nav-ctn">
+        <ul className={isMenuOpen ? "showMenu nav-list" : "nav-list"}>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/profile">Profile</a>
+          </li>
+          <li>
+            <a href="/login">Log in</a>
+          </li>
+          <li>
+            <a href="/signup">Sign up</a>
+          </li>
+        </ul>
+
+        <button className="hamburger" onClick={toggleMenu}>
+          {isMenuOpen ? (
+            <i className="menuIcon material-icons">close</i>
+          ) : (
+            <i className="menuIcon material-icons">menu</i>
+          )}
+        </button>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
