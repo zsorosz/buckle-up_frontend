7019f695425b56294b9cc2210561453ce8de3db1
Yunus Ozbay <yunus.ozbay@hotmail.com>
2023-05-31 13:55:50 +0200

    my trips page done

--- src/pages/MyTrips.tsx
+++ src/pages/MyTrips.tsx
@@ -1,48 +1,36 @@
 import { useContext } from "react";
 import { SessionContext } from "../contexts/SessionContext";
-import { Activities } from "../components/NewTripForm";
-import { City } from "../components/Map";
+import { Trip } from "../types";
 
 function MyTrips(): JSX.Element {
-
   const { userData, refreshData } = useContext(SessionContext);
   console.log(userData?.trips);
-  const trips: Trips = userData?.trips
+  const trips: Trip[] | undefined = userData?.trips;
 
-  type Trips = {
-    _id: string,
-    title: string,
-    startingCity: string,
-    destination: string,
-    createdAt: string,
-    updatedAt: string,
-    totalDistance: number,
-    totalTime: number,
-    attractions: Activities[],
-    waypoints: City[]
-  }
 
   return (
-    <>
-    {trips?.map(trip=>(
-    <article className="card">
-      <img
-        className="card__background"
-        src={`https://source.unsplash.com/600x300/?${trip.destination}`}
-        alt="destination photo"
-        width="1920"
-        height="2193"
-      />
-      <div className="card__content | flow">
-        <div className="card__content--container | flow">
-          <h2 className="card__title">{trip.destination}</h2>
-          <p className="card__description">The trip from {trip.startingCity}<br></br>to {trip.destination}</p>
-        </div>
-        <button className="card__button">See details</button>
-      </div>
-    </article>
-    ))}
-    </>
+    <div className="cards-container">
+      {trips?.map((trip) => (
+        <article className="card">
+          <img
+            src={`https://source.unsplash.com/600x300/?${trip.destination}`}
+            alt="destination photo"
+            width="1920"
+            height="2193"
+          />
+          <div className="card-details">
+            <h2 className="card-title">{trip.destination}</h2>
+            <p className="card-description">
+              The trip from {trip.startingCity}
+              <br></br>to {trip.destination}
+            </p>
+            <div className="primary-btn">
+            <a href={`/${trip._id}`}>See details</a>
+            </div> 
+          </div>
+        </article>
+      ))}
+    </div>
   );
 }
 
---
+++ src/types.ts
@@ -0,0 +1,21 @@
+import { Activities } from "./components/NewTripForm";
+import { City } from "./components/Map";
+
+export interface UserData {
+  username: string;
+  email: string;
+  trips: Trip[];
+}
+
+export interface Trip {
+  _id: string;
+  title: string;
+  startingCity: string;
+  destination: string;
+  createdAt: string;
+  updatedAt: string;
+  totalDistance: number;
+  totalTime: number;
+  attractions: Activities[];
+  waypoints: City[];
+}
