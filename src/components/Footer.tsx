79bc95ca78dc34d785653d231b10f5f738c20dd3
Yunus Ozbay <yunus.ozbay@hotmail.com>
2023-06-05 21:39:15 +0200

    footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,79 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faTwitter,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
 function Footer(): JSX.Element {
-  return <footer className="footer-ctn">Footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="footer-col">
+            <h4>company</h4>
+            <ul>
+              <li>
+                <a href="#">our story</a>
+              </li>
+              <li>
+                <a href="#">our services</a>
+              </li>
+              <li>
+                <a href="#">benefits</a>
+              </li>
+              <li>
+                <a href="#">team</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>get help</h4>
+            <ul>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+              <li>
+                <a href="#">Subscription options</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>legal</h4>
+            <ul>
+              <li>
+                <a href="#">terms & conditions</a>
+              </li>
+              <li>
+                <a href="#">privacy policy</a>
+              </li>
+              <li>
+                <a href="#">terms of use</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>follow us</h4>
+            <div className="social-links">
+              <a href="#">
+                <FontAwesomeIcon icon={faFacebookF} />
+              </a>
+              <a href="#">
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+              <a href="#">
+                <FontAwesomeIcon icon={faInstagram} />
+              </a>
+              <a href="#">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
