179e01e2c20c8d067f21738752bc949e382df525
zsorosz <oroszi.zsofi@gmail.com>
2025-01-31 14:33:21 +0100

    use public images in destination cards

--- src/components/PopularDestinations.tsx
+++ src/components/PopularDestinations.tsx
@@ -2,15 +2,6 @@ import Card from "./Card";
 
 function PopularDestinations(): JSX.Element {
   const destinations: string[] = ["Berlin", "Paris", "Barcelona", "Rome"];
-  const images = {
-    Berlin:
-      "https://cdn.pixabay.com/photo/2017/12/31/10/26/berlin-3051937_1280.jpg",
-    Paris:
-      "https://cdn.pixabay.com/photo/2018/04/06/17/17/paris-3296269_1280.jpg",
-    Barcelona:
-      "https://images.ctfassets.net/bth3mlrehms2/2T2y3fo3Z60l0XXMwjtstO/01970b699f8c96c39c7ed4b489d9ebab/Sagrada_Familia_Barcelona.jpg?w=3841&h=2160&fl=progressive&q=50&fm=jpg",
-    Rome: "https://cdn.pixabay.com/photo/2019/10/06/08/57/tiber-river-4529605_1280.jpg",
-  };
 
   return (
     <section className="popular-destinations-ctn">
@@ -22,7 +13,7 @@ function PopularDestinations(): JSX.Element {
             key={city}
             title={city}
             link={`/destination/${city}`}
-            image={images[city as keyof typeof images]}
+            image={`/${city}.jpg`}
           />
         ))}
       </div>
